4409c7d65d27677acaafb19de90f638750078668
nadeem <nadeemsangrasi903@gmail.com>
2025-02-16 22:10:20 +0500

    deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import ContactSection from "@/components/sections/ContactSection";
 import FeaturesSection from "@/components/sections/FeatureSection";
 import HeroSection from "@/components/sections/HeroSection";
 import PricingSection from "@/components/sections/PricingSection";
-import Image from "next/image";
 
 export default function Home() {
   return (
